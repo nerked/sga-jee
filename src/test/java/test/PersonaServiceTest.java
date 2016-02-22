ead26b17fb5ddaef9055666cfa7d7002d220b367
Rober <oliroberto@gmail.com>
2016-02-22 23:27:25 +0100

    Added embbebed container

--- src/test/java/test/PersonaServiceTest.java
+++ src/test/java/test/PersonaServiceTest.java
@@ -2,9 +2,13 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,14 +16,21 @@ import org.junit.Test;
 import com.prueba.sga.domain.Persona;
 import com.prueba.sga.servicio.PersonaService;
 
+
 public class PersonaServiceTest {
 
 	private PersonaService personaService;
+	private static EJBContainer contenedor;
+	private static Context contexto;
 	
 	@Before
 	public void setUp() throws Exception{
-//		EJBContainer contenedor = EJBContainer.createEJBContainer();
-//		personaService = (PersonaService) contenedor.getContext().lookup("java:global/classes/PersonaServiceImpl!com.prueba.sga.servicio.PersonaServicio");
+		Map<String, Object> properties = new HashMap<String, Object>();
+	    properties.put(EJBContainer.MODULES, new File("target/classes"));
+		contenedor = EJBContainer.createEJBContainer(properties);
+		contexto = contenedor.getContext();
+		EJBContainer contenedor = EJBContainer.createEJBContainer();
+		personaService = (PersonaService) contenedor.getContext().lookup("java:global/classes/PersonaServiceImpl!com.prueba.sga.servicio.PersonaServicio");
 	}
 
 	@Test
